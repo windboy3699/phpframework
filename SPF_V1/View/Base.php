4cdf87dc6c277d74ebd800d29132e7833982a25f
潘晓栋 <pennypan@pptv.com>
2018-04-11 11:10:26 +0800

    mv v1

--- SPF_V1/View/Base.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * @package /Class
- * @author  XiaodongPan
- * @version $Id: Base.php 2016-11-04 $
- */
-require_once dirname(dirname(__FILE__)) . '/Lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
